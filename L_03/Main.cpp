66871d1033c1896bbd85cc876501824b7bd32689
iXmalMonk <ixmal999@gmail.com>
2022-11-24 21:04:44 +0300

    image

---
+++ L_03/Main.cpp
@@ -0,0 +1,32 @@
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+int main()
+{
+	RenderWindow window(VideoMode(1100, 925), "L_03");
+
+	Image image;
+	image.loadFromFile("github.jpg");
+
+	Texture texture;
+	texture.loadFromImage(image);
+
+	Sprite sprite;
+	sprite.setTexture(texture);
+	sprite.setPosition(0, 0);
+
+	Event event;
+
+	while (window.isOpen())
+	{
+		while (window.pollEvent(event))
+			if (event.type == Event::Closed)
+				window.close();
+
+		window.clear();
+		window.draw(sprite);
+		window.display();
+	}
+	return 0;
+}
