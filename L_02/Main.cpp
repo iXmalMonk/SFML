65945c64ee091c18627160569fbf3a205d68ea89
iXmalMonk <ixmal999@gmail.com>
2022-11-24 23:44:17 +0300

    rect, keyboard, mouse

---
+++ L_02/Main.cpp
@@ -0,0 +1,60 @@
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+int main()
+{
+	RenderWindow window(VideoMode(1280, 720), "SFML");
+
+	Image heroImage;
+	heroImage.loadFromFile("images/hero.png");
+
+	Texture heroTexture;
+	heroTexture.loadFromImage(heroImage);
+
+	Sprite heroSprite;
+	heroSprite.setTexture(heroTexture);
+	heroSprite.setTextureRect(IntRect(0, 0, 64, 64));
+	heroSprite.setPosition(100, 100);
+
+	Event event;
+
+	while (window.isOpen())
+	{
+		while (window.pollEvent(event))
+			if (event.type == Event::Closed)
+				window.close();
+
+		if (Keyboard::isKeyPressed(Keyboard::W))
+		{
+			heroSprite.move(0, -0.1);
+			heroSprite.setTextureRect(IntRect(0, 0, 64, 64));
+		}
+		if (Keyboard::isKeyPressed(Keyboard::S))
+		{
+			heroSprite.move(0, 0.1);
+			heroSprite.setTextureRect(IntRect(0, 64*3, 64, 64));
+		}
+		if (Keyboard::isKeyPressed(Keyboard::A))
+		{
+			heroSprite.move(-0.1, 0);
+			heroSprite.setTextureRect(IntRect(0, 64*2, 64, 64));
+		}
+		if (Keyboard::isKeyPressed(Keyboard::D))
+		{
+			heroSprite.move(0.1, 0);
+			heroSprite.setTextureRect(IntRect(0, 64, 64, 64));
+		}
+
+		if (Mouse::isButtonPressed(Mouse::Left))
+			heroSprite.setColor(Color::Red);
+		else
+			heroSprite.setColor(Color::White);
+
+		window.clear();
+		window.draw(heroSprite);
+		window.display();
+	}
+
+	return 0;
+}
